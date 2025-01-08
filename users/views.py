a8081df2a234cc6dabbdf692096e44551422c29c
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-01-08 16:07:33 -0600

    [FEATURE]: Add user registration endpoint with password validation and JWT token generation

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.throttling import UserRateThrottle
+from .serializers import UserSerializer
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.core.exceptions import ValidationError
 
-# Create your views here.
+class CreateUserView(APIView):
+    throttle_classes = [UserRateThrottle]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            password = serializer.validated_data['password']
+            try:
+                validate_password(password)
+            except ValidationError as e:
+                return Response({"error": e.messages}, status=400)
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "message": "User created successfully!",
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
+            }, status=201)
+        return Response(serializer.errors, status=400)

8121474abe6b1fe81acd92741fc19f7c86de3539
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-01-08 16:16:21 -0600

    [UPDATE]: Add ChangePasswordView and RefreshTokenView

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateUserView,TokenView, LogoutView,UserProfileView
+from .views import CreateUserView,TokenView, LogoutView,UserProfileView,ChangePasswordView,RefreshTokenView
 
 urlpatterns = [
     path('create-user/', CreateUserView.as_view(), name='create-user'),

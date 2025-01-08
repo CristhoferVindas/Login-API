2532405c590c4adccd5286f734036e64b8d556e7
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-01-08 16:09:19 -0600

    [FEATURE]: Add user login endpoint with JWT token generation

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateUserView
+from .views import CreateUserView,TokenView 
 
 urlpatterns = [
     path('create-user/', CreateUserView.as_view(), name='create-user'),
+    path('token/', TokenView.as_view(), name='token'),
 ]
--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.password_validation import validate_password
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.core.exceptions import ValidationError
+from .models import User
 
 class CreateUserView(APIView):
     throttle_classes = [UserRateThrottle]
@@ -25,3 +26,24 @@ class CreateUserView(APIView):
                 "refresh": str(refresh),
             }, status=201)
         return Response(serializer.errors, status=400)
+
+class TokenView(APIView):
+    throttle_classes = [UserRateThrottle]
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        if not email or not password:
+            return Response({"error": "Email and password are required"}, status=400)
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    'access': str(refresh.access_token),
+                    'refresh': str(refresh),
+                })
+            else:
+                return Response({"error": "Invalid credentials"}, status=400)
+        except User.DoesNotExist:
+            return Response({"error": "Invalid credentials"}, status=400)
